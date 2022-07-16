c1b4cc040d573ddeda848667fcaf56e056dba3b0
Ed de Almeida <edvaldoajunior@gmail.com>
2022-07-16 14:01:28 -0300

    Acrescentando express e dotenv

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world with Typescript' })
+})
+
+app.use(route)
+
+
+app.listen(process.env.PORT, () => 'server running on port 3333')
